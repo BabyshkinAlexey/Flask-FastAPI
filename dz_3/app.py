3117cd0f8ac8aaf46ede5103bc0a40d13f4e15c7
Alexey <mr.alexv2000@mail.ru>
2024-03-25 21:51:06 +0500

    dz_3

--- dz_3/app.py
+++ dz_3/app.py
@@ -6,6 +6,16 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///mydatabase.db'
 db.init_app(app)
 
+category = [
+    {"title": 'All books', "func_name": 'get_all_books'},
+]
+
+
+@app.route('/')
+
+@app.route('/index/')
+def index():
+    return render_template('index.html', category=category)
 
 @app.cli.command("init-db")
 def init_db():
@@ -13,19 +23,24 @@ def init_db():
     print('Все таблицы созданы успешно!')
     
 
-@app.cli.command("add-books")
+@app.cli.command("fill-books")
 def fill_tables():
     count = 5
     for author in range(count):
         new_author = Author(name=f'Author{author}', surname=f'Surname{author}')
         db.session.add(new_author)
     db.session.commit()
-    # Добавляем книги
     for book in range(1, count ** 2):
         author = choice(range(1, count+1))
         new_book = Book(title=f'Title{book}', year=choice(range(1900, 2020)),author=author)
         db.session.add(new_book)
     db.session.commit()
+    
+@app.route('/all_books/')
+def get_all_books():
+    books = Book.query.all()
+    context = {'books': books}
+    return render_template('all_books.html', **context)
 
 if __name__ == '__main__':
     app.run(debug=True)
